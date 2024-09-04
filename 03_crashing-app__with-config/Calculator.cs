f702668143ab3b2148ba10b6055dc784f07baab5
Jan Krivanek <jankrivanek@microsoft.com>
2024-09-04 12:56:53 +0200

    Sample 3

---
+++ 03_crashing-app__with-config/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace DotUtils;
+
+public static class Calculator
+{
+    public static int Sum(params string[] numbers)
+        => Enumerable.Sum(numbers.Cast<int>());
+}
---
+++ 03_crashing-app__with-config/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DotUtils;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Give a space separated list of numbers to sum:");
+        Calculator.Sum(Console.ReadLine()!.Split());
+        Console.WriteLine($"Result: " );
+    }
+}
